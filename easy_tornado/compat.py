f84ffe1641a27d7c46ceb6d6a1f5af3775be68cf
王树根 <wsg1107556314@163.com>
2018-11-14 11:24:27 +0800

    maxint兼容性
    Signed-off-by: 王树根 <wsg1107556314@163.com>

--- easy_tornado/compat.py
+++ easy_tornado/compat.py
@@ -2,9 +2,13 @@
 # author: 王树根
 # email: wangshugen@ict.ac.cn
 # date: 2018/11/14 10:53
+import sys
+
 import six
 
 if six.PY2:
     C_StandardError = StandardError
+    C_MAXINT = sys.maxint
 if six.PY3:
     C_StandardError = BaseException
+    C_MAXINT = sys.maxsize
