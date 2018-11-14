60c016d840a3d38fd747e3e1040ca955fe3a39a4
王树根 <wsg1107556314@163.com>
2018-11-14 10:25:37 +0800

    修正了remove_file不能删除文件的bug
    Signed-off-by: 王树根 <wsg1107556314@163.com>

--- easy_tornado/utils/file_utils.py
+++ easy_tornado/utils/file_utils.py
@@ -31,32 +31,55 @@ def dirname(path):
 
 
 # 判断文件是否存在
-def file_exists(_file):
-    return os.path.exists(_file)
+def file_exists(file_path):
+    return os.path.exists(file_path)
 
 
-# 获取文件大小
-def get_file_size(_file):
-    return os.path.getsize(_file)
+def get_file_size(file_path):
+    """
+    获取文件大小
+    :param file_path: 文件路径
+    :return: 文件大小
+    """
+    return os.path.getsize(file_path)
 
 
 # 获取文件行数
-def get_file_lines(_file):
-    if not file_exists(_file):
+def get_file_lines(file_path):
+    if not file_exists(file_path):
         return -1
-    lines = os.popen(r"wc -l %s | awk '{print $1}'" % _file).read()
+    lines = os.popen(r"wc -l %s | awk '{print $1}'" % file_path).read()
     return int(lines)
 
 
-# 移除文件
-def remove_file(_file):
-    shutil.rmtree(_file, ignore_errors=True)
+def remove_file(file_path, ignore_errors=True):
+    """
+    移除文件
+    :param file_path: 文件路径
+    """
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+    elif os.path.isdir(file_path):
+        shutil.rmtree(file_path, ignore_errors=ignore_errors)
+    else:
+        raise ValueError('file_path should be either a file or directory')
 
 
-# 准备目录
-def create_if_not_exist_path(target_path):
-    if not os.path.exists(target_path):
-        os.makedirs(target_path)
+def create_if_not_exists(file_path):
+    """
+    如果对应路径不存在的话，则进行创建
+    :param file_path: 文件路径
+    """
+    if os.path.exists(file_path) and not os.path.isdir(file_path):
+        raise ValueError('file_path {} exists but is not a derectory')
+
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
+
+@deprecated(new_fn=create_if_not_exists)
+def create_if_not_exist_path(file_path):
+    create_if_not_exists(file_path)
 
 
 # 拼接路径
@@ -73,22 +96,31 @@ def format_path(base_path, sub_path):
     return concat_path(base_path, sub_path)
 
 
-# 判断是否为绝对路径
 def is_abspath(file_path):
+    """
+    判断是否为绝对路径
+    :param file_path: 文件路径
+    :return: 若为绝对路径返回True, 否则返回False
+    """
     return file_path is not None and isinstance(file_path, str) and file_path.startswith('/')
 
 
-# 修正路径(将holder中key对应的值增加base_path)
 def refine_path(base_path, holder, key):
+    """
+    修正路径(将holder中key对应的值增加base_path)
+    :param base_path: 基本路径
+    :param holder: 路径容器
+    :param key: 路径键或索引
+    """
     if isinstance(holder, dict):
         assert key in holder
     elif isinstance(holder, list):
         assert isinstance(key, int) and 0 <= key < len(holder)
     else:
         raise TypeError('holder should be either dict or list, but got {}'.format(type(holder)))
-    if is_abspath(holder[key]):
-        return
-    holder[key] = concat_path(base_path, holder[key])
+
+    if not is_abspath(holder[key]):
+        holder[key] = cp(base_path, holder[key])
 
 
 def file_md5sum(file_path):
--- easy_tornado/utils/monitor_utils.py
+++ easy_tornado/utils/monitor_utils.py
@@ -10,17 +10,22 @@ from .file_utils import load_file_contents
 from .file_utils import remove_file
 
 
-def kill_process(pid_file_path, signum=None):
-    if not file_exists(pid_file_path):
+def kill_process(pid_path, signum=None):
+    if not file_exists(pid_path):
         return None
+
     if signum is None:
         signum = signal.SIGKILL
+
     # 读取PID文件
-    pid = load_file_contents(pid_file_path)[0].strip()
+    pid = int(load_file_contents(pid_path)[0].strip())
+
     # 向该进程发送信号
-    cmd_stop_mt = 'kill -{} {}'.format(signum, pid)
-    cmd_stop_mt_sts = subprocess.call(cmd_stop_mt, shell=True)
-    if cmd_stop_mt_sts == 0:
-        remove_file(pid_file_path)
-        return True
-    return False
+    try:
+        kill(pid, signum)
+    except OSError as e:
+        if len(e.args) != 2 or e.args[1] != 'No such process':
+            raise
+
+    # 移除PID文件
+    remove_file(pid_path)
