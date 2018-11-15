882304644de51f826996024687494999cfebdf02
王树根 <wsg1107556314@163.com>
2018-11-15 09:28:01 +0800

    更新了依赖包及提供的包
    Signed-off-by: 王树根 <wsg1107556314@163.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # author: 王树根
 # email: wangshugen@ict.ac.cn
 # date: 2018/8/23 14:29
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='easy_tornado',
       version='0.4.4',
@@ -11,5 +11,5 @@ setup(name='easy_tornado',
       author='empire (Wang Shugen)',
       author_email='wsg1107556314@163.com',
       url='https://lib.wshugen.cn/',
-      packages=['easy_tornado', 'easy_tornado.utils', 'easy_tornado.web'],
+      packages=find_packages(),
       install_requires=['tornado', 'decorator', 'six', 'urllib3'])
