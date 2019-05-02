25232632f59a13139407d3d1184e1bee78e91844
王树根 <wsg1107556314@163.com>
2019-05-02 17:01:23 +0800

    更新版本号为0.5.4
    Signed-off-by: 王树根 <wsg1107556314@163.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 ssl._create_default_https_context = ssl._create_unverified_context
 
 setup(name='easy_tornado',
-      version='0.5.3',
+      version='0.5.4',
       description='A tornado based web framework package',
       author='empire (Wang Shugen)',
       author_email='wsg1107556314@163.com',
