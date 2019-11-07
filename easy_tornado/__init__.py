cc36d5351fbdd92b7b301ab1f9eb0b37b80bf698
王树根 <wsg1107556314@163.com>
2019-11-07 17:45:01 +0800

    导出filtering函数到easy_tornado
    Signed-off-by: 王树根 <wsg1107556314@163.com>

--- easy_tornado/__init__.py
+++ easy_tornado/__init__.py
@@ -4,3 +4,5 @@ from .compat import cse_message
 from .compat import happy_move_functions
 from .compat import utf8decode
 from .compat import utf8encode
+from .filtering import filter_dict_object
+from .filtering import filter_module_methods
