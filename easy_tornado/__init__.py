277fa779c306ed2fe8171e0d62c5382ac1bda93d
王树根 <wsg1107556314@163.com>
2019-11-07 17:45:47 +0800

    导出functional函数到easy_tornado
    Signed-off-by: 王树根 <wsg1107556314@163.com>

--- easy_tornado/__init__.py
+++ easy_tornado/__init__.py
@@ -6,3 +6,7 @@ from .compat import utf8decode
 from .compat import utf8encode
 from .filtering import filter_dict_object
 from .filtering import filter_module_methods
+from .functional import async_call
+from .functional import deprecated
+from .functional import none_context
+from .functional import timed
