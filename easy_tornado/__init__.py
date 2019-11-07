5c077e131e609d82c45bdabc2c5c9d6d595ce43d
王树根 <wsg1107556314@163.com>
2019-11-07 17:44:11 +0800

    导出compat函数到easy_tornado
    Signed-off-by: 王树根 <wsg1107556314@163.com>

---
+++ easy_tornado/__init__.py
@@ -0,0 +1,6 @@
+from .compat import C_MAXINT
+from .compat import C_StandardError
+from .compat import cse_message
+from .compat import happy_move_functions
+from .compat import utf8decode
+from .compat import utf8encode
