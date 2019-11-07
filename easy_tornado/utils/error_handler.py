c12b09cfa22301e929253f37d2c032038b51cd58
王树根 <wsg1107556314@163.com>
2019-11-07 16:34:48 +0800

    增强了error_handler功能
    1.增加了ErrorInternal类
    2.增加了raise_print/exit_print函数
    3.废弃error_exit函数
    
    Signed-off-by: 王树根 <wsg1107556314@163.com>

--- easy_tornado/utils/error_handler.py
+++ easy_tornado/utils/error_handler.py
@@ -3,7 +3,27 @@
 # email: wangshugen@ict.ac.cn
 # date: 2018/11/19 11:03
 from .logging import it_print
+from ..compat import C_StandardError
 from ..functional import deprecated
+
+
+class InternalError(C_StandardError):
+
+    def __init__(self, *args, **kwargs):
+        super(InternalError, self).__init__(*args, **kwargs)
+
+
+def raise_print(*args, **kwargs):
+    """
+    print message and raise InternalError
+    :param args: see it_print
+    :param kwargs: it_print
+    """
+    if 'device' not in kwargs:
+        kwargs['device'] = 2
+    it_print(*args, **kwargs)
+    message = kwargs.pop('message', 'Unknown')
+    raise InternalError(message)
 
 
 def exit_print(*args, **kwargs):
