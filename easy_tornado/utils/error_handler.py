87d828d8f8c591e6483fafd998ac64eb49fec0f3
王树根 <wsg1107556314@163.com>
2019-11-07 16:26:56 +0800

    新增exit_print函数: 打印并终止程序
    Signed-off-by: 王树根 <wsg1107556314@163.com>

--- easy_tornado/utils/error_handler.py
+++ easy_tornado/utils/error_handler.py
@@ -3,14 +3,28 @@
 # email: wangshugen@ict.ac.cn
 # date: 2018/11/19 11:03
 from .logging import it_print
+from ..functional import deprecated
 
 
+def exit_print(*args, **kwargs):
+    """
+    print message and exit
+    :param args: see it_print
+    :param kwargs: see it_print
+    """
+    if 'device' not in kwargs:
+        kwargs['device'] = 2
+    it_print(*args, **kwargs)
+    errno = kwargs.pop('errno', 0)
+    exit(int(errno))
+
+
+@deprecated(new_fn=exit_print)
 def error_exit(errno=0, error=None):
     """
-    打印消息并退出
-    :param errno: 错误码
-    :param error: 错误内容
-    :return:
+    print message and exit
+    :param errno: error code
+    :param error: error message
     """
     if error is not None:
         it_print(error)
